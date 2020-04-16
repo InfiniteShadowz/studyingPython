989a45872ed77f7a9d214e02b403b33dd5e802d7
Fernanda Shinoda <fernanda.shinoda@hotmail.com>
2020-04-16 12:45:29 -0300

    consumption

---
+++ URIjudge/beginner/1014.py
@@ -0,0 +1,12 @@
+#Python3
+
+//input
+distTotal = int(input())
+combustGasto = float(input())
+
+//processamento
+//consumoMedio = [km/l]
+consumMedio = dist/combust
+
+//Output format
+print('{:.3f} km/l'.format(consum))
