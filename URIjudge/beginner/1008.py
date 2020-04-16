0ca7365500e8a45527882e0541c64093625afc93
Fernanda Shinoda <fernanda.shinoda@hotmail.com>
2020-04-16 12:24:07 -0300

    salary

---
+++ URIjudge/beginner/1008.py
@@ -0,0 +1,13 @@
+#Python3
+
+#input 
+numFunc = int(input())
+horasTrabalh = int(input())
+valorHora = float(input())
+
+#processing
+salario = horasTrabalh * valorHora
+
+#OUTPUT
+print("NUMBER =", numFunc)
+print("SALARY = U$ {:.2f}".format(salario))
