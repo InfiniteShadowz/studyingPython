58ddad4884ce0caac204b962c723757c21bc5937
Fernanda Shinoda <fernanda.shinoda@hotmail.com>
2020-04-16 12:05:04 -0300

    area of a circle

---
+++ URIjudge/beginner/1002.py
@@ -0,0 +1,14 @@
+  
+#Python3
+
+#n = PI
+n = 3.14159
+
+#input ponto flutuante
+raio = float(input())
+
+#processamento
+area = n*raio**2
+
+#output com format de 4 casas
+print("A={:.4f}".format(area))  
