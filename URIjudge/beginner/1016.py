d6d2b007f2014aef48e807c0381348dfd314b56d
Fernanda Shinoda <fernanda.shinoda@hotmail.com>
2020-04-16 12:58:17 -0300

    distance

---
+++ URIjudge/beginner/1016.py
@@ -0,0 +1,11 @@
+#Python3
+
+#input
+dist = int(input())
+
+#processamento
+#o tempo levado para realizar distancia x será sempre o dobro da distancia, pois a cada 2 min se distancia 1 km
+tempo = dist*2
+
+#output
+print('{} minutos'.format(tempo))
