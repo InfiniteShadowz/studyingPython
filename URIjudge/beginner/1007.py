335e19f4511ca2b6f82f515cf3968178536ff080
Fernanda Shinoda <fernanda.shinoda@hotmail.com>
2020-04-16 12:22:34 -0300

    subtraction

---
+++ URIjudge/beginner/1007.py
@@ -0,0 +1,13 @@
+#Python3
+
+#input int
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+#processing
+DIFERENCA = (A * B - C * D)
+
+#OUTPUT
+print("DIFERENCA =", DIFERENCA)
