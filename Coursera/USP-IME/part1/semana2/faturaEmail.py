5279e65227377cbc14d041f2398cea36eaa919c4
Fernanda Shinoda <63424056+InfiniteShadowz@users.noreply.github.com>
2020-04-18 16:33:15 -0300

    Create faturaEmail.py

---
+++ Coursera/USP-IME/part1/semana2/faturaEmail.py
@@ -0,0 +1,7 @@
+nome = input()
+diaVenc = input()
+mesVenc = input()
+
+valorFatura = input()
+
+print("Olá,", nome, "\nA sua fatura com vencimento em", diaVenc, "de", mesVenc, "no valor de R$", valorFatura, "está fechada.")
