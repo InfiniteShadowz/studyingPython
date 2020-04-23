1e842efe5910b1da69aabd4cb0d5f72c26fc9e7b
Fernanda Shinoda <63424056+InfiniteShadowz@users.noreply.github.com>
2020-04-23 20:53:18 -0300

    Create nImpares.py

---
+++ Coursera/USP-IME/part1/Semana4/nImpares.py
@@ -0,0 +1,18 @@
+#Python3
+
+n = int(input("Digite o valor de n: "))
+count = 1
+impar = 0
+while impar != n:
+    if count % 2 != 0:
+        print(count)
+        impar += 1
+    count += 1
+
+
+
+
+    
+
+
+
