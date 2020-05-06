090a532ba97b20c06588cb9233ff73d27a79d0a2
Fernanda Shinoda <63424056+InfiniteShadowz@users.noreply.github.com>
2020-05-06 19:31:02 -0300

    Create funcVogal.py

---
+++ Coursera/USP-IME/part1/semana5/funcVogal.py
@@ -0,0 +1,10 @@
+#Python3
+
+def vogal(c):
+    if c == "a" or c == "A" or c == "i" or c == "I" or c == "u" or c == "U" or c == "e" or c == "E" or c == "o" or c == "O":
+        return True
+    else:
+        return False
+
+    
+    
