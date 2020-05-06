7e3dbf1b2e13e7d0bfcd4e61eda4d7fc9e352795
Fernanda Shinoda <63424056+InfiniteShadowz@users.noreply.github.com>
2020-05-06 20:06:37 -0300

    Create funcFizzBuzz.py

---
+++ Coursera/USP-IME/part1/semana5/funcFizzBuzz.py
@@ -0,0 +1,13 @@
+#Python3
+
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return n
+    
+        
